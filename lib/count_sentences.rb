8ce8372d6ed86103a2acd98fbc3283f8fce4609d
Omayma Mahjoub <omayma.mahjoub@ensi-uma.tn>
2020-07-13 11:52:20 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+    return self.end_with?("!")||self.end_with?(".")||self.end_with?("?")
   end
 
   def question?
-
+    return self.end_with?("?")
   end
 
   def exclamation?
-
+    return self.end_with("!")
   end
 
   def count_sentences
-
+    count=0
+    self.each do |car|
+      if car=="!" || car=="?" ||car=="."
+        count+=1
+      end
+    end
   end
 end
