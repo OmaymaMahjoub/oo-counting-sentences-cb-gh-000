5d2d6a066c59e8f994c359bf13c48970be1f765a
Omayma Mahjoub <omayma.mahjoub@ensi-uma.tn>
2020-07-13 11:57:30 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    
+    return self.end_with?("!")||self.end_with?(".")||self.end_with?("?")
   end
 
   def question?
-
+    return self.end_with?("?")
   end
 
   def exclamation?
-
+    return self.end_with?("!")
   end
 
   def count_sentences
-
+    count=0
+    arr=self.split(" ")
+    arr.each do |car|
+      if car.end_with?("!") || car.end_with?("?") ||car.end_with?(".")
+        count+=1
+      end
+    end
+    return count
   end
 end
